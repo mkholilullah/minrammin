bfd8b57224deae475e7996e23b60e101ac7c7fa6
roamercodes <mkholilullah29@gmail.com>
2025-01-20 23:31:42 +0700

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export interface User {
+    fullName: string;
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+    profilePicture: string;
+    isActive: string;
+    activationCode: string;
+}

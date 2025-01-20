eb6929e72487eecdd13c7bce7c2b54e858e7c595
roamercodes <mkholilullah29@gmail.com>
2025-01-20 23:50:27 +0700

    update user model:create validation

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,6 +7,51 @@ export interface User {
     password: string;
     role: string;
     profilePicture: string;
-    isActive: string;
+    isActive: boolean;
     activationCode: string;
 }
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema<User>({
+    fullName: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    username: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    email: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    password: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    role: {
+        type: Schema.Types.String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+    profilePicture: {
+        type: Schema.Types.String,
+        default: "user.jpg",
+    },
+    isActive: {
+        type: Schema.Types.Boolean,
+        default: false,
+    },
+    activationCode: {
+        type: Schema.Types.String,
+    }
+},
+    {
+        timestamps: true,
+    }
+);
+
+const UserModel = mongoose.model("User", UserSchema);
+
+export default UserModel;
